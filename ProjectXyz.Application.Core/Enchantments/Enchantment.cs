1438369e5278f7c9228931a42daca32d618ad20e
Nick Cosentino <n.b.cosentino@gmail.com>
2014-11-12 20:03:52 -0500

    [Enchantments] Enchantments Copy Data From Lower Layer
    - Prepping to make the data layer interfaces immutable.
    - Enchantments will now copy the data layer information to operate on their own copy of it.

--- ProjectXyz.Application.Core/Enchantments/Enchantment.cs
+++ ProjectXyz.Application.Core/Enchantments/Enchantment.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics.Contracts;
 
-using ProjectXyz.Data.Interface.Enchantments.ExtensionMethods;
+using ProjectXyz.Application.Interface.Enchantments.ExtensionMethods;
 using ProjectXyz.Application.Interface.Enchantments;
 
 namespace ProjectXyz.Application.Core.Enchantments
@@ -14,7 +14,13 @@ namespace ProjectXyz.Application.Core.Enchantments
     {
         #region Fields
         private readonly IEnchantmentContext _context;
-        private readonly ProjectXyz.Data.Interface.Enchantments.IEnchantment _enchantment;
+        private readonly string _statId;
+        private readonly double _value; 
+        private readonly string _calculationId; 
+        private readonly string _trigger; 
+        private readonly string _statusType;
+        
+        private TimeSpan _remainingDuration;
         #endregion
 
         #region Constructors
@@ -24,39 +30,44 @@ namespace ProjectXyz.Application.Core.Enchantments
             Contract.Requires<ArgumentNullException>(enchantment != null);
 
             _context = context;
-            _enchantment = enchantment;
+            _statId = enchantment.StatId;
+            _value = enchantment.Value;
+            _calculationId = enchantment.CalculationId;
+            _trigger = enchantment.Trigger;
+            _statusType = enchantment.StatusType;
+            _remainingDuration = enchantment.RemainingDuration;
         }
         #endregion
 
         #region Properties
         public string StatId
         {
-            get { return _enchantment.StatId; }
+            get { return _statId; }
         }
 
         public double Value
         {
-            get { return _enchantment.Value; }
+            get { return _value; }
         }
 
         public string CalculationId
         {
-            get { return _enchantment.CalculationId; }
+            get { return _calculationId; }
         }
 
         public string Trigger
         {
-            get { return _enchantment.Trigger; }
+            get { return _trigger; }
         }
 
         public string StatusType
         {
-            get { return _enchantment.StatusType; }
+            get { return _statusType; }
         }
 
         public TimeSpan RemainingDuration
         {
-            get { return _enchantment.RemainingDuration; }
+            get { return _remainingDuration; }
         }
         #endregion
 
@@ -71,13 +82,13 @@ namespace ProjectXyz.Application.Core.Enchantments
 
         public void UpdateElapsedTime(TimeSpan elapsedTime)
         {
-            if (_enchantment.HasInfiniteDuration())
+            if (this.HasInfiniteDuration())
             {
                 return;
             }
 
-            _enchantment.RemainingDuration = TimeSpan.FromMilliseconds(Math.Max(
-                (_enchantment.RemainingDuration - elapsedTime).TotalMilliseconds,
+            _remainingDuration = TimeSpan.FromMilliseconds(Math.Max(
+                (_remainingDuration - elapsedTime).TotalMilliseconds,
                 0));
         }
         #endregion
