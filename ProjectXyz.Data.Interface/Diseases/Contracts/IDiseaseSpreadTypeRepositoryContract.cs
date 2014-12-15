590846a629ffbea84c9cdbe7d73c7b253d8fbfbc
Nick Cosentino <n.b.cosentino@gmail.com>
2014-12-14 23:31:28 -0500

    - Contracts
    - Implementations

---
+++ ProjectXyz.Data.Interface/Diseases/Contracts/IDiseaseSpreadTypeRepositoryContract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace ProjectXyz.Data.Interface.Diseases.Contracts
+{
+    [ContractClassFor(typeof(IDiseaseSpreadTypeRepository))]
+    public abstract class IDiseaseSpreadTypeRepositoryContract : IDiseaseSpreadTypeRepository
+    {
+        #region Methods
+        public IDiseaseSpreadType GetById(Guid id)
+        {
+            Contract.Requires<ArgumentException>(id != Guid.Empty);
+            Contract.Ensures(Contract.Result<IDiseaseSpreadType>() != null);
+
+            return default(IDiseaseSpreadType);
+        }
+        #endregion
+    }
+}
---
+++ ProjectXyz.Data.Sql/Diseases/DiseaseSpreadTypeRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+using ProjectXyz.Data.Interface.Diseases;
+
+namespace ProjectXyz.Data.Sql.Diseases
+{
+    public sealed class DiseaseSpreadTypeRepository : IDiseaseSpreadTypeRepository
+    {
+        #region Fields
+        private readonly IDatabase _database;
+        private readonly IDiseaseSpreadTypeFactory _diseaseSpreadTypeFactory;
+        #endregion
+
+        #region Constructors
+        private DiseaseSpreadTypeRepository(
+            IDatabase database,
+            IDiseaseSpreadTypeFactory diseaseSpreadTypeFactory)
+        {
+            Contract.Requires<ArgumentNullException>(database != null);
+            Contract.Requires<ArgumentNullException>(diseaseSpreadTypeFactory != null);
+
+            _database = database;
+            _diseaseSpreadTypeFactory = diseaseSpreadTypeFactory;
+        }
+        #endregion
+
+        #region Methods
+        public static IDiseaseSpreadTypeRepository Create(
+            IDatabase database,
+            IDiseaseSpreadTypeFactory diseaseSpreadTypeFactory)
+        {
+            Contract.Requires<ArgumentNullException>(database != null);
+            Contract.Requires<ArgumentNullException>(diseaseSpreadTypeFactory != null);
+            Contract.Ensures(Contract.Result<IDiseaseSpreadTypeRepository>() != null);
+
+            return new DiseaseSpreadTypeRepository(
+                database,
+                diseaseSpreadTypeFactory);
+        }
+
+        public IDiseaseSpreadType GetById(Guid id)
+        {
+            const string QUERY = @"
+                SELECT
+                    *
+                FROM
+                    [DiseaseSpreadTypes]
+                WHERE
+                    Id = @Id
+                ;";
+
+            using (var reader = _database.Query(QUERY, "@Id", id))
+            {
+                if (!reader.Read())
+                {
+                    throw new InvalidOperationException("No disease spread type with Id '" + id + "' was found.");
+                }
+
+                var name = reader.GetString(reader.GetOrdinal("Name"));
+                return _diseaseSpreadTypeFactory.Create(
+                    id,
+                    name);
+            }
+        }
+        #endregion
+    }
+}
