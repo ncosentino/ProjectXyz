ee2db0990c0dabe4d1068edbdaf1271cbe3bf6d5
Nick Cosentino <n.b.cosentino@gmail.com>
2015-11-14 16:46:26 -0500

    Create Character Request
    - Adding in a new message type.

---
+++ ProjectXyz.Api.Messaging.Core/Initialization/CreateCharacterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ProjectXyz.Api.Messaging.Interface;
+
+namespace ProjectXyz.Api.Messaging.Core.Initialization
+{
+    public sealed class CreateCharacterRequest : BaseRequest
+    {
+        #region Properties
+        public string Name { get; set; }
+
+        public bool Male { get; set; }
+
+        public Guid RaceId { get; set; }
+
+        public Guid ClassId { get; set; }
+        #endregion
+    }
+}
