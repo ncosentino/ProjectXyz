4b8d40b8d2b3ff774ee9c87df1ddc921e744de9b
Nick Cosentino <n.b.cosentino@gmail.com>
2018-03-13 00:55:41 -0400

    EventArgs Wrapper Framework

---
+++ ProjectXyz.Api.Framework/Events/EventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectXyz.Api.Framework.Events
+{
+    public sealed class EventArgs<TData> : EventArgs
+    {
+        public EventArgs(TData data)
+        {
+            Data = data;
+        }
+
+        public TData Data { get; }
+    }
+}
