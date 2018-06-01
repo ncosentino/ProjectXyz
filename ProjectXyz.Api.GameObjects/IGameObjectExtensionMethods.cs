a03cd0c4a18bcb753813f41709e8f311a39636ab
ncosentino <n.b.cosentino@gmail.com>
2018-05-31 20:53:32 -0400

    issue #24: Extension Methods on IGameObject for Getting Behaviors
    - https://bitbucket.org/nexuslabs/projectxyz/issues/24
    - Added some extension methods for working with IBehaviors and IGameObjects.

---
+++ ProjectXyz.Api.GameObjects/IGameObjectExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectXyz.Api.Behaviors;
+
+namespace ProjectXyz.Api.GameObjects
+{
+    public static class IGameObjectExtensionMethods
+    {
+        public static IEnumerable<TBehavior> Get<TBehavior>(this IGameObject gameObject)
+            where TBehavior : IBehavior
+        {
+            return gameObject
+                .Behaviors
+                .Get<TBehavior>();
+        }
+
+        public static bool Has<TBehavior>(this IGameObject gameObject)
+            where TBehavior : IBehavior
+        {
+            return gameObject
+                .Behaviors
+                .Get<TBehavior>()
+                .Any();
+        }
+
+        public static TBehavior GetOnly<TBehavior>(this IGameObject gameObject)
+            where TBehavior : IBehavior
+        {
+            return gameObject
+                .Behaviors
+                .Get<TBehavior>()
+                .Single();
+        }
+    }
+}
