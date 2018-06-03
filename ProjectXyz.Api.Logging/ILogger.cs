97fde9b62bd864cdbdff0162af3cd470121532c7
ncosentino <n.b.cosentino@gmail.com>
2018-06-03 01:11:04 -0400

    issue #26: Add a basic logging API
    - https://bitbucket.org/nexuslabs/projectxyz/issues/26
    - Added an API project for logging that has an ILogger.

---
+++ ProjectXyz.Api.Logging/ILogger.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectXyz.Api.Logging
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+
+        void Debug(string message, object data);
+
+        void Info(string message);
+
+        void Info(string message, object data);
+
+        void Warn(string message);
+
+        void Warn(string message, object data);
+
+        void Error(string message);
+
+        void Error(string message, object data);
+    }
+}
