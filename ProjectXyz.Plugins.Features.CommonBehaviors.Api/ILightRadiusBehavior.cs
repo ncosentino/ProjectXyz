175c6ab41845048c112de4c3e9630dd995958b7d
ncosentino <n.b.cosentino@gmail.com>
2018-06-22 11:13:09 -0400

    issue #46: Create Light Radius Behavior
    - https://bitbucket.org/nexuslabs/projectxyz/issues/46
    - Added a simple behavior for light radius.

---
+++ ProjectXyz.Plugins.Features.CommonBehaviors.Api/ILightRadiusBehavior.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectXyz.Plugins.Features.CommonBehaviors.Api
+{
+    public interface ILightRadiusBehavior : IReadOnlyLightRadiusBehavior
+    {
+        new double Intensity { get; set; }
+
+        new double Radius { get; set; }
+
+        new double Blue { get; set; }
+
+        new double Red { get; set; }
+
+        new double Green { get; set; }
+    }
+}
---
+++ ProjectXyz.Plugins.Features.CommonBehaviors.Api/IReadOnlyLightRadiusBehavior.cs
@@ -0,0 +1,17 @@
+﻿using ProjectXyz.Api.Behaviors;
+
+namespace ProjectXyz.Plugins.Features.CommonBehaviors.Api
+{
+    public interface IReadOnlyLightRadiusBehavior : IBehavior
+    {
+        double Intensity { get; }
+
+        double Radius { get; }
+
+        double Blue { get; }
+
+        double Red { get; }
+
+        double Green { get; }
+    }
+}
