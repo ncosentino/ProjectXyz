1005169bbac37f70e4eea87fd2e4686378fbfc7b
ncosentino <n.b.cosentino@gmail.com>
2018-12-06 15:36:28 -0500

    [Fix] Allow randomization with more than one pick when generating items
    - Previous implementation inside of the ItemGeneratorFacade would pick one of the filtered
      generators and then force it to generate the items to match the count. The new approach
      is to keep picking a random filtered generator and asking it to generate at least one item up
      to the remaining number. This allows more generators to participate if they match.

--- ProjectXyz.Shared.Game.Items.Generation.InMemory/ItemGeneratorFacade.cs
+++ ProjectXyz.Shared.Game.Items.Generation.InMemory/ItemGeneratorFacade.cs
@@ -7,6 +7,7 @@ using ProjectXyz.Api.GameObjects;
 using ProjectXyz.Api.GameObjects.Generation;
 using ProjectXyz.Api.GameObjects.Generation.Attributes;
 using ProjectXyz.Plugins.Features.GameObjects.Items.Api.Generation;
+using ProjectXyz.Shared.Game.GameObjects.Generation;
 
 namespace ProjectXyz.Plugins.Features.GameObjects.Items.Generation.InMemory
 {
@@ -27,18 +28,36 @@ namespace ProjectXyz.Plugins.Features.GameObjects.Items.Generation.InMemory
 
         public IEnumerable<IGameObject> GenerateItems(IGeneratorContext generatorContext)
         {
-            var filteredGenerators = _attributeFilterer.Filter(
-                _itemGenerators,
-                generatorContext);
-            var generator = filteredGenerators.RandomOrDefault(_randomNumberGenerator);
-            if (generator == null)
+            var filteredGenerators = _attributeFilterer
+                .Filter(
+                    _itemGenerators,
+                    generatorContext)
+                .ToArray();
+            if (!filteredGenerators.Any())
             {
                 throw new InvalidOperationException(
                     $"There are no item generators that match the context '{generatorContext}'.");
             }
 
-            var generatedItems = generator.GenerateItems(generatorContext);
-            return generatedItems;
+            var totalCount = GetCount(
+                generatorContext.MinimumGenerateCount,
+                generatorContext.MaximumGenerateCount);
+            var generatedCount = 0;
+            while (generatedCount < totalCount)
+            {
+                var generator = filteredGenerators.RandomOrDefault(_randomNumberGenerator);
+                var currentContext = new GeneratorContext(
+                    1,
+                    totalCount - generatedCount,
+                    generatorContext.Attributes);
+
+                var generatedItems = generator.GenerateItems(currentContext);
+                foreach (var generatedItem in generatedItems)
+                {
+                    generatedCount++;
+                    yield return generatedItem;
+                }
+            }
         }
 
         public IEnumerable<IGeneratorAttribute> SupportedAttributes => _itemGenerators
@@ -48,6 +67,16 @@ namespace ProjectXyz.Plugins.Features.GameObjects.Items.Generation.InMemory
         public void Register(IItemGenerator itemGenerator)
         {
             _itemGenerators.Add(itemGenerator);
+        }
+
+        private int GetCount(
+            int itemCountMinimum,
+            int itemCountMaximum)
+        {
+            var count = _randomNumberGenerator.NextInRange(
+                itemCountMinimum,
+                itemCountMaximum);
+            return count;
         }
     }
 }
