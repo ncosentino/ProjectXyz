e5ea45ddf3724b7fd36e5008114f28bf604b4d69
ncosentino <n.b.cosentino@gmail.com>
2021-02-21 20:28:14 -0800

    EnchantmentFactory -> Behavior Owners

--- ProjectXyz.Shared.Game.GameObjects.Enchantments/EnchantmentFactory.cs
+++ ProjectXyz.Shared.Game.GameObjects.Enchantments/EnchantmentFactory.cs
@@ -1,24 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using ProjectXyz.Api.Behaviors;
 using ProjectXyz.Api.Enchantments;
+using ProjectXyz.Api.Enchantments.Calculations;
 
 namespace ProjectXyz.Shared.Game.GameObjects.Enchantments
 {
     public sealed class EnchantmentFactory : IEnchantmentFactory
     {
+        private readonly IBehaviorManager _behaviorManager;
+
+        public EnchantmentFactory(IBehaviorManager behaviorManager)
+        {
+            _behaviorManager = behaviorManager;
+        }
+
         public IEnchantment Create(IEnumerable<IBehavior> behaviors)
         {
-            var hasStatDefinitionIdBehavior = behaviors
+            if (behaviors
                 .TakeTypes<IHasStatDefinitionIdBehavior>()
-                .SingleOrDefault();
-            if (hasStatDefinitionIdBehavior == null)
+                .SingleOrDefault() == null)
             {
                 throw new ArgumentException($"Missing required '{typeof(IHasStatDefinitionIdBehavior)}'.");
             }
 
+            if (behaviors
+                .TakeTypes<IEnchantmentTargetBehavior>()
+                .SingleOrDefault() == null)
+            {
+                throw new ArgumentException($"Missing required '{typeof(IEnchantmentTargetBehavior)}'.");
+            }
+
             var enchantment = new Enchantment(behaviors);
+            _behaviorManager.Register(enchantment, enchantment.Behaviors);
             return enchantment;
         }
     }
