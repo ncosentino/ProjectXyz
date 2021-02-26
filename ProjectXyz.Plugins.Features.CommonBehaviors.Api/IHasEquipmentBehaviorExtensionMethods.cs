fdd7237f4916ae576df8c552af61df036a5edc89
ncosentino <n.b.cosentino@gmail.com>
2021-02-25 18:26:36 -0800

    IHasEquipmentBehaviorExtensionMethods
    - These should help. Yay.

---
+++ ProjectXyz.Plugins.Features.CommonBehaviors.Api/IHasEquipmentBehaviorExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ProjectXyz.Api.Framework;
+using ProjectXyz.Api.GameObjects;
+
+namespace ProjectXyz.Plugins.Features.CommonBehaviors.Api
+{
+    public static class IHasEquipmentBehaviorExtensionMethods
+    {
+        public static IEnumerable<ICanBeEquippedBehavior> GetCanBeEquippedBehaviors(this IHasEquipmentBehavior hasEquipmentBehavior)
+        {
+            return hasEquipmentBehavior
+                .GetCanBeEquippedBehaviorsBySlot()
+                .Select(x => x.Item2);
+        }
+
+        public static IEnumerable<Tuple<IIdentifier, ICanBeEquippedBehavior>> GetCanBeEquippedBehaviorsBySlot(this IHasEquipmentBehavior hasEquipmentBehavior)
+        {
+            foreach (var equipSlotId in hasEquipmentBehavior.SupportedEquipSlotIds)
+            {
+                if (!hasEquipmentBehavior.TryGet(
+                    equipSlotId,
+                    out var canBeEquippedBehavior))
+                {
+                    continue;
+                }
+
+                yield return Tuple.Create(
+                    equipSlotId,
+                    canBeEquippedBehavior);
+            }
+        }
+
+        public static IEnumerable<Tuple<IIdentifier, IGameObject>> GetEquippedItemsBySlot(this IHasEquipmentBehavior hasEquipmentBehavior)
+        {
+            return hasEquipmentBehavior
+                .GetCanBeEquippedBehaviorsBySlot()
+                .Select(x => Tuple.Create(
+                    x.Item1,
+                    (IGameObject)x.Item2.Owner)); // FIXME: wtf is this shitty casting
+        }
+
+        public static IEnumerable<IGameObject> GetEquippedItems(this IHasEquipmentBehavior hasEquipmentBehavior)
+        {
+            return hasEquipmentBehavior
+                .GetCanBeEquippedBehaviors()
+                .Select(x => (IGameObject)x.Owner); // FIXME: wtf is this shitty casting
+        }
+    }
+}
