077a1a99324881fc96a350c3a18b8e311b243ebf
ncosentino <n.b.cosentino@gmail.com>
2021-02-28 16:06:45 -0800

    RPG-30: Experiment with enchantment that can modify the weather
    - Moved some classes from game to backend library for re-use.

---
+++ ProjectXyz.Application.Stats.Core/StatDefinitionToTermMapping.cs
@@ -0,0 +1,19 @@
+﻿
+using ProjectXyz.Api.Framework;
+using ProjectXyz.Api.Stats;
+
+namespace ProjectXyz.Plugins.Stats
+{
+    public sealed class StatDefinitionToTermMapping : IStatDefinitionToTermMapping
+    {
+        public StatDefinitionToTermMapping(IIdentifier stateDefinitionId, string term)
+        {
+            StatDefinitionId = stateDefinitionId;
+            Term = term;
+        }
+
+        public IIdentifier StatDefinitionId { get; }
+
+        public string Term { get; }
+    }
+}
