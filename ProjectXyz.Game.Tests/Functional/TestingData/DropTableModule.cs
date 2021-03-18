c672e1c3aa5365de28ee7b5d53b78ff7e0e6c047
ncosentino <n.b.cosentino@gmail.com>
2021-03-17 19:54:54 -0700

    File Rename

--- ProjectXyz.Game.Tests/Functional/TestingData/DropTableModule.cs
+++ ProjectXyz.Game.Tests/Functional/TestingData/DropTableModule.cs
@@ -1,9 +1,5 @@
 ﻿using Autofac;
 
-using ProjectXyz.Api.Framework;
-using ProjectXyz.Plugins.Features.GameObjects.Items.Api.Generation.DropTables;
-using ProjectXyz.Shared.Framework;
-
 namespace ProjectXyz.Game.Tests.Functional.TestingData
 {
     public sealed class DropTableModule : Module
@@ -16,10 +12,5 @@ namespace ProjectXyz.Game.Tests.Functional.TestingData
                 .AsImplementedInterfaces()
                 .SingleInstance();
         }
-    }
-
-    public sealed class DropTableIdentifiers : IDropTableIdentifiers
-    {
-        public IIdentifier FilterContextDropTableIdentifier { get; } = new StringIdentifier("drop-table");
     }
 }
