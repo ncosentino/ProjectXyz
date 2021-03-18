1c8a59ea594e8f20a7386b6008309305d0bc789d
ncosentino <n.b.cosentino@gmail.com>
2021-03-18 12:52:43 -0700

    IActorIdentifiers -> Animations

--- ProjectXyz.Plugins.Features.GameObjects.Actors.Api/IActorIdentifiers.cs
+++ ProjectXyz.Plugins.Features.GameObjects.Actors.Api/IActorIdentifiers.cs
@@ -7,5 +7,21 @@ namespace ProjectXyz.Plugins.Features.GameObjects.Actors.Api
         IIdentifier FilterContextActorStatsIdentifier { get; }
 
         IIdentifier ActorTypeIdentifier { get; }
+
+        IIdentifier AnimationStandBack { get; }
+
+        IIdentifier AnimationStandForward { get; }
+
+        IIdentifier AnimationStandLeft { get; }
+
+        IIdentifier AnimationStandRight { get; }
+
+        IIdentifier AnimationWalkBack { get; }
+
+        IIdentifier AnimationWalkForward { get; }
+
+        IIdentifier AnimationWalkLeft { get; }
+
+        IIdentifier AnimationWalkRight { get; }
     }
 }
