77a50604d61db32c3a6cfb3a102f37dd1c7a9045
ncosentino <n.b.cosentino@gmail.com>
2021-03-26 00:07:16 -0700

    "ThreadSafe" MapGameObjectManager
    - Feels gross but... this seems to work?

--- ProjectXyz.Plugins.Features.Mapping.Default/MapGameObjectManager.cs
+++ ProjectXyz.Plugins.Features.Mapping.Default/MapGameObjectManager.cs
@@ -28,9 +28,12 @@ namespace ProjectXyz.Plugins.Features.Mapping.Default
 
         public void MarkForRemoval(IEnumerable<IGameObject> gameObjects)
         {
-            foreach (var gameObject in gameObjects)
+            lock (_gameObjectsToRemove)
             {
-                _gameObjectsToRemove.Add(gameObject);
+                foreach (var gameObject in gameObjects)
+                {
+                    _gameObjectsToRemove.Add(gameObject);
+                }
             }
         }
 
@@ -39,25 +42,49 @@ namespace ProjectXyz.Plugins.Features.Mapping.Default
 
         public void MarkForAddition(IEnumerable<IGameObject> gameObjects)
         {
-            foreach (var gameObject in gameObjects)
+            lock (_gameObjectsToAdd)
             {
-                _gameObjectsToAdd.Add(gameObject);
+                foreach (var gameObject in gameObjects)
+                {
+                    _gameObjectsToAdd.Add(gameObject);
+                }
             }
         }
 
         public void Synchronize()
         {
             var requiredSync = false;
-            foreach (var gameObject in _gameObjectsToAdd)
+
+            var addedGameObjects = new HashSet<IGameObject>();
+            lock (_gameObjectsToAdd)
             {
-                _gameObjects.Add(gameObject);
-                requiredSync = true;
+                lock (_gameObjects)
+                {
+                    foreach (var gameObject in _gameObjectsToAdd)
+                    {
+                        _gameObjects.Add(gameObject);
+                        addedGameObjects.Add(gameObject);
+                        requiredSync = true;
+                    }
+                }
+
+                _gameObjectsToAdd.Clear();
             }
 
-            foreach (var gameObject in _gameObjectsToRemove)
+            var removedGameObjects = new HashSet<IGameObject>();
+            lock (_gameObjectsToRemove)
             {
-                _gameObjects.Remove(gameObject);
-                requiredSync = true;
+                lock (_gameObjects)
+                {
+                    foreach (var gameObject in _gameObjectsToRemove)
+                    {
+                        _gameObjects.Remove(gameObject);
+                        removedGameObjects.Add(gameObject);
+                        requiredSync = true;
+                    }
+                }
+
+                _gameObjectsToRemove.Clear();
             }
 
             if (requiredSync)
@@ -65,12 +92,9 @@ namespace ProjectXyz.Plugins.Features.Mapping.Default
                 Synchronized?.Invoke(
                     this,
                     new GameObjectsSynchronizedEventArgs(
-                        _gameObjectsToAdd,
-                        _gameObjectsToRemove));
+                        addedGameObjects,
+                        removedGameObjects));
             }
-
-            _gameObjectsToAdd.Clear();
-            _gameObjectsToRemove.Clear();
         }
     }
 }
