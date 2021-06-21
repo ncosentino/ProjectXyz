839e9887b4de3087a4565f0e36bc7c9c9249cc83
Nick Cosentino <n.b.cosentino@gmail.com>
2021-06-20 21:23:06 -0700

    Null Contract -> ElapsedTurnsTriggerSourceMechanicRegistrar
    - Help catch a sneaky null exception... Doesn't fix but helps us catch.

--- ProjectXyz.Plugins.Features.TurnBased/ElapsedTurnsTriggerSourceMechanicRegistrar.cs
+++ ProjectXyz.Plugins.Features.TurnBased/ElapsedTurnsTriggerSourceMechanicRegistrar.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 
+using NexusLabs.Contracts;
+
 using ProjectXyz.Api.Triggering;
 using ProjectXyz.Plugins.Features.TurnBased.Api;
 
@@ -16,21 +18,33 @@ namespace ProjectXyz.Plugins.Features.TurnBased
 
         public bool CanRegister(ITriggerMechanic triggerMechanic)
         {
+            Contract.RequiresNotNull(
+                triggerMechanic,
+                $"{nameof(triggerMechanic)} cannot be null.");
             return triggerMechanic is IElapsedTurnsTriggerMechanic;
         }
 
         public void RegisterTrigger(ITriggerMechanic triggerMechanic)
         {
+            Contract.RequiresNotNull(
+                triggerMechanic,
+                $"{nameof(triggerMechanic)} cannot be null.");
             _elapsedTimeTriggerMechanics.Add((IElapsedTurnsTriggerMechanic)triggerMechanic);
         }
 
         public void UnregisterTrigger(ITriggerMechanic triggerMechanic)
         {
+            Contract.RequiresNotNull(
+                triggerMechanic,
+                $"{nameof(triggerMechanic)} cannot be null.");
             _elapsedTimeTriggerMechanics.Remove((IElapsedTurnsTriggerMechanic)triggerMechanic);
         }
 
         public void Update(ITurnInfo turnInfo)
         {
+            Contract.RequiresNotNull(
+                turnInfo,
+                $"{nameof(turnInfo)} cannot be null.");
             _elapsedTimeTriggerMechanics.RemoveAll(x => !x.Update(turnInfo));
         }
     }
